5f76db000aa1b3d21e7b96bb6e4af6fb042658e5
joakimthun <joakimthun@gmail.com>
2014-02-19 22:26:31 +0100

    added bootstrap, and some more angular files

---
+++ ShoppingWhiz.Web/js/app.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+
+var shoppingWhiz = angular.module('shoppingWhiz', ['ngRoute', 'ngResource']);
+
+shoppingWhiz.config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+        .when('/',
+            {
+                controller: 'AccountController',
+                templateUrl: 'js/templates/login.html'
+            })
+        .when('/signup',
+            {
+                controller: 'AccountController',
+                templateUrl: 'js/templates/signup.html'
+            })
+        .when('/shoppingList',
+            {
+                controller: 'ShoppingListController',
+                templateUrl: 'js/templates/shoppingList.html'
+            })
+        .when('/newItem',
+            {
+                controller: 'ShoppingListController',
+                templateUrl: 'js/templates/newPurchase.html'
+            })
+        .otherwise({ redirectTo: '/' });
+
+        //$locationProvider.html5Mode(true);
+    });
